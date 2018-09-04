648942b2be889e3bbdc70ab03b24ad6f097eba1c
Ankur <Ankur@192.168.0.27>
2018-09-04 15:49:56 -0700

    Used Math utility.

--- AreaOfCircle/src/Area/area.java
+++ AreaOfCircle/src/Area/area.java
@@ -6,14 +6,13 @@ public class area {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String reply;
-		double pie=3.14;
 		int radius;
 		double area;
 		System.out.println("How are you doing today?");
 		reply = s.next();
 		System.out.println("What is the radius of the circle?");
 		radius=s.nextInt();
-		area=pie*radius*radius;
+		area=Math.PI*radius*radius;
 		System.out.println("Good to hear that you are doing " +reply +"\nThe area of circle is " +area);
 	}
 
